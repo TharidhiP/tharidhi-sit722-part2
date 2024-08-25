bdb94f7cec57a96365298a290b6c2d26eed55dc5
Tharidhi Perera <61136228+TharidhiP@users.noreply.github.com>
2024-08-25 15:36:05 +1000

    Update config.py

--- book_catalog/config.py
+++ book_catalog/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("postgresql://sit722_week6_user:DVVbPrxfI3ta80cpVAq5xdFnoBeevrPj@dpg-cr2ae22j1k6c73ekigig-a.oregon-postgres.render.com/sit722_week6")
+    DATABASE_URL: str = os.getenv("DATABASE_URL","postgresql://sit722_week6_user:DVVbPrxfI3ta80cpVAq5xdFnoBeevrPj@dpg-cr2ae22j1k6c73ekigig-a.oregon-postgres.render.com/sit722_week6")
 
 settings = Settings()
